13da30f157e4195f06f53667c076328b152344e7
jewel-2004 <jeweljoseph947@GMAIL.COM>
2025-11-01 19:19:19 +0530

    Fix Signup route path to be case-sensitive

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -4,7 +4,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
-import Signup from "./components/signup";
+import Signup from "./components/Signup";
 import Home from "./components/home";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/Signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
